65c49542d56df17f2e29892026a189735d24cb20
kimura-khamali <kimurakhamali@gmail.com>
2024-03-25 09:13:14 +0300

    function

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+/*fun sumOfWeight(weights: Array<Double>: Array<Double>): Double {
+    return weights.sum()
+}
+fun averageweight(weights: Array<Double>): Double: {
+    var sum= sumOfWeight(weights)
+    var avg=sum/33
+    return avg
+}*/
+
+//write a function that takes in 2 integers and returns
+//the square of their sum
+
+fun sumOfInterger(num1: Int , num2: Int): Int{
+    //var num1: Int = 4
+    //var num2: Int = 8
+    var sum = num1 + num2
+    var square = sum * sum
+    return square
+}
+
+
